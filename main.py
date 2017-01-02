ac21c7b9cc6420b5824a153d7d6844be497f01e6
jaccarmac <jaccarmac@gmail.com>
2017-01-02 20:36:54 +0000

    Rudimentary tokenization.
    FossilOrigin-Name: 888382bab802c31e37c2950c188e3560dfc97a38

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
+def tokenize(line):
+    space_sep = line.split()
+    parsed = []
+    for token in space_sep:
+        if token.endswith("'"):
+            parsed.append(parsed.pop() + ' ' + token)
+        else:
+            parsed.append(token)
+    return parsed
+
 for line in open('main.st'):
-    print(line.split())
+    print(tokenize(line))
